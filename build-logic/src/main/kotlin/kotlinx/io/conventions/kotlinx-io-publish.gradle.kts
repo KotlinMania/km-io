2516af294a782dd007f7df53fa151e101ca357e9
Sydney Renee <sydney@solace.ofharmony.ai>
2026-05-24 13:55:31 -0700

    Align km-io publishing workflows

--- build-logic/src/main/kotlin/kotlinx/io/conventions/kotlinx-io-publish.gradle.kts
+++ build-logic/src/main/kotlin/kotlinx/io/conventions/kotlinx-io-publish.gradle.kts
@@ -3,103 +3,56 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENCE file.
  */
 
-import org.gradle.jvm.tasks.Jar
-import java.net.URI
-
 plugins {
-    `maven-publish`
-    signing
+    id("com.vanniktech.maven.publish")
 }
 
-publishing {
-    repositories {
-        configureMavenPublication(project)
+val publishedArtifactId =
+    if (project.name == "km-io-core") {
+        "km-io"
+    } else {
+        project.name
     }
 
-    val javadocJar = project.configureEmptyJavadocArtifact()
-    publications.withType(MavenPublication::class).all {
-        pom.configureMavenCentralMetadata(project)
-        signPublicationIfKeyPresent(project, this)
-        artifact(javadocJar)
+mavenPublishing {
+    publishToMavenCentral()
+    if (project.hasSigningKey()) {
+        signAllPublications()
     }
 
-    tasks.withType<PublishToMavenRepository>().configureEach {
-        dependsOn(tasks.withType<Sign>())
-    }
-}
+    coordinates(project.group.toString(), publishedArtifactId, project.version.toString())
 
-fun MavenPom.configureMavenCentralMetadata(project: Project) {
-    name = project.name
-    description = "IO support for Kotlin"
-    url = "https://github.com/Kotlin/kotlinx-io"
+    pom {
+        name.set(publishedArtifactId)
+        description.set("Multiplatform Kotlin IO primitives, ByteString support, and Okio adapters")
+        inceptionYear.set("2026")
+        url.set("https://github.com/KotlinMania/km-io")
 
-    licenses {
-        license {
-            name = "Apache-2.0"
-            url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
-            distribution = "repo"
-        }
-    }
-
-    developers {
-        developer {
-            id = "JetBrains"
-            name = "JetBrains Team"
-            organization = "JetBrains"
-            organizationUrl = "https://www.jetbrains.com"
-        }
-    }
-
-    scm {
-        url = "https://github.com/Kotlin/kotlinx-io"
-    }
-}
-
-
-fun RepositoryHandler.configureMavenPublication(project: Project) {
-    val repositoryUrl = project.getSensitiveProperty("libs.repo.url")
-    if (!repositoryUrl.isNullOrBlank()) {
-        maven {
-            url = uri(repositoryUrl)
-            credentials {
-                username = project.getSensitiveProperty("libs.repo.user")
-                password = project.getSensitiveProperty("libs.repo.password")
+        licenses {
+            license {
+                name.set("Apache-2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("repo")
             }
         }
-    }
 
-    // Something that's easy to "clean" for development not mavenLocal
-    maven(project.rootProject.layout.buildDirectory.dir("repo")) {
-        name = "buildRepo"
-    }
-}
-
-fun Project.configureEmptyJavadocArtifact(): Jar {
-    val javadocJar by project.tasks.creating(Jar::class) {
-        archiveClassifier = "javadoc"
-        // contents are deliberately left empty
-    }
-    return javadocJar
-}
-
-fun signPublicationIfKeyPresent(project: Project, publication: MavenPublication) {
-    val keyId = project.getSensitiveProperty("libs.sign.key.id")
-    val signingKey = project.getSensitiveProperty("libs.sign.key.private")
-    val signingKeyPassphrase = project.getSensitiveProperty("libs.sign.passphrase")
-    if (!signingKey.isNullOrBlank()) {
-        project.extensions.configure<SigningExtension>("signing") {
-            useInMemoryPgpKeys(keyId, signingKey, signingKeyPassphrase)
-            sign(publication)
-
-            // Temporary workaround, see https://github.com/gradle/gradle/issues/26091#issuecomment-1722947958
-            tasks.withType<AbstractPublishToMaven>().configureEach {
-                val signingTasks = tasks.withType<Sign>()
-                mustRunAfter(signingTasks)
+        developers {
+            developer {
+                id.set("sydneyrenee")
+                name.set("Sydney Renee")
+                email.set("sydney@solace.ofharmony.ai")
+                url.set("https://github.com/sydneyrenee")
             }
+        }
+
+        scm {
+            url.set("https://github.com/KotlinMania/km-io")
+            connection.set("scm:git:git://github.com/KotlinMania/km-io.git")
+            developerConnection.set("scm:git:ssh://github.com/KotlinMania/km-io.git")
         }
     }
 }
 
-fun Project.getSensitiveProperty(name: String): String? {
-    return project.findProperty(name) as? String ?: System.getenv(name)
-}
+fun Project.hasSigningKey(): Boolean =
+    !(findProperty("signingInMemoryKey") as? String).isNullOrBlank() ||
+        !System.getenv("ORG_GRADLE_PROJECT_signingInMemoryKey").isNullOrBlank()
